e5871afed78111d0c7668862e31586563ae42786
Ashley Mical <ashleymmical@gmail.com>
2024-01-03 10:17:01 -0600

    created credits-page view and added router link

--- src/App.vue
+++ src/App.vue
@@ -11,6 +11,7 @@ import HelloWorld from './components/HelloWorld.vue';
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/credits">Credits</RouterLink>
       </nav>
     </div>
   </header>
---
+++ src/views/CreditsView.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+//import TheWelcome from '../components/TheWelcome.vue';
+</script>
+
+<template>
+  <div class="credits">
+    <h1>This is the credits page</h1>
+  </div>
+</template>
+  
+<style>
+</style>
+  
