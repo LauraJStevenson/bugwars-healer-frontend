2469a4428610a3ad7916bd2bc02cf85a84bbbda6
Ashley Mical <ashleymmical@gmail.com>
2024-01-12 15:07:00 -0600

    add dynamic links for login, register, settings, and play

--- src/components/GlobalNav.vue
+++ src/components/GlobalNav.vue
@@ -1,36 +1,51 @@
-<script setup lang="ts"></script>
-
 <template>
   <div class="global-nav-bar">
     <ul>
       <li><img src="/public/images/Logo.png" /></li>
 
       <li><RouterLink active-class="active" to="/">Home</RouterLink></li>
-      <li><RouterLink active-class="active" to="/gamelobby">Play</RouterLink></li>
+
+      <li v-if="isAuthenticated">
+        <RouterLink active-class="active" to="/gamelobby">Play</RouterLink>
+      </li>
+
       <li><RouterLink active-class="active" to="/howtoplay">How to Play</RouterLink></li>
+
       <li><RouterLink active-class="active" to="/credits">Credits</RouterLink></li>
+
       <li>
         <RouterLink active-class="active" to="/behindthescenes">Behind the Scenes</RouterLink>
       </li>
-      <li>
-        <!--<li v-if="isLoggedIn" @click="settings">Settings</li>-->
+
+      <li v-if="isAuthenticated">
         <RouterLink active-class="active" to="/settings">Settings</RouterLink>
       </li>
-      <li><RouterLink active-class="active" to="/login">Login</RouterLink></li>
-      <li><RouterLink active-class="active" to="/register">Register</RouterLink></li>
 
-      <!--<li v-if="isLoggedIn" @click="logout">Logout</li>
-      <li v-else></li>
-      <li>
-        <RouterLink to="/login">Login</RouterLink>
-        <RouterLink to="/register">Register</RouterLink>
+      <li v-if="!isAuthenticated">
+        <RouterLink active-class="active" to="/login">Login</RouterLink>
       </li>
-      -->
+
+      <li v-if="!isAuthenticated">
+        <RouterLink v-if="!isAuthenticated" active-class="active" to="/register"
+          >Register</RouterLink
+        >
+      </li>
     </ul>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script>
+import { useAuthStore } from '../stores/auth';
+
+export default {
+  computed: {
+    isAuthenticated() {
+      const authStore = useAuthStore();
+      return authStore.isAuthenticated;
+    },
+  },
+};
+</script>
 
 <style scoped>
 .global-nav-bar {
