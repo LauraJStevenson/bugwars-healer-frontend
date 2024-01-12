a1b128f884743dffabe7628b159397eb0f9e0152
Laura Stevenson <laura.j.stevenson05@gmail.com>
2024-01-12 11:26:01 -0700

    added instructions to create .env file to avoid CORS errors when working locally

--- src/config/axios.ts
+++ src/config/axios.ts
@@ -6,7 +6,9 @@ export interface RetryAxiosRequestConfig extends AxiosRequestConfig {
 
 export function configureAxios() {
   axios.defaults.baseURL = import.meta.env.VITE_REMOTE_API;
-  //When we deploy to render we can use this as an environment variable, create .env file in root directory
+  //When we deploy to render we can use this as an environment variable,
+  //create .env file in root directory declaring variable VITE_REMOTE_API = http://localhost:8080/api/v1
+  //no brackets or quotes, just simple text
   //vite will package the project into vanilla javascript
   axios.defaults.headers['Content-Type'] = 'application/json';
 }
