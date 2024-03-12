7c93ba173b8a88f5c9ba6f9232f9cf84568253f7
Ashley Mical <108195754+micamash@users.noreply.github.com>
2024-03-12 13:50:57 -0500

    progress on game displaying map

--- src/views/GamePlayView.vue
+++ src/views/GamePlayView.vue
@@ -1,77 +1,48 @@
+
+
 <template>
   <div v-if="currentMapCells" class="game-play">
-
-    <!-- Score Tracker -->
     <div class="score-tracker">
-      <div>Bug Team 1 Score: {{ gameStore.scores.team1 }} </div>
+      <div>Bug Team 1 Score: {{ gameStore.scores.team1 }}</div>
       <div>Bug Team 2 Score: {{ gameStore.scores.team2 }}</div>
     </div>
 
-    <!-- Time Slider -->
     <div class="time-slider-div">
-      <input class="time-slider" type="range" min="0" :max="gameStore.ticks" v-model="currentTick" @input="updateCurrentTick" />
+      <input type="range" min="0" :max="gameStore.ticks" v-model="currentTick" @input="updateCurrentTick" class="time-slider" />
     </div>
 
     <div class="game-box">
-
-      <!-- Arrow Left for Previous Map -->
       <button @click="previousMap">Previous</button>
 
-      <!-- Game Map-->
       <div class="game-map">
         <div v-for="(row, rowIndex) in currentMapCells" :key="rowIndex" class="row">
-        <div v-for="(cell, cellIndex) in row" :key="cellIndex" class="cell">
-          <img v-if="cell.image" :src="cell.image" :alt="cell.type" :class="{'bug': cell.type === 'Bug', [cell.direction.toLowerCase()]: cell.type === 'Bug'}" />
-
-            <template v-if="cell.type === 'Bug'">
-              Bug at {{ cell.x }},{{ cell.y }}
-            
-              
-              <span v-if="getScriptName({ x: cell.x, y: cell.y, type: cell.type })"> {{ getScriptName({ x: cell.x, y: cell.y, type: cell.type }) }} </span>
-
-            </template>
-            <template v-else>
-              {{ cell.type }} at {{ cell.x }},{{ cell.y }}
-            </template>
+          <div v-for="(cell, cellIndex) in row" :key="cellIndex" class="cell">
+            <img v-if="cell.image" :src="cell.image" :alt="cell.type" :class="{'bug': cell.type === 'Bug', [cell.direction.toLowerCase()]: cell.type === 'Bug'}" />
           </div>
         </div>
       </div>
 
-      <!-- Arrow Right for Next Map -->
       <button @click="nextMap">Next</button>
-
     </div>
 
-    <!-- Script Selections-->
     <div class="script-selections">
-
-      <!-- Script selector for the first bug -->
       <div class="script-selector">
         <select v-model="selectedScriptIndex1" id="selectedScriptIndex1">
-          <option v-for="(script, index) in scripts" :key="index" :value="index">
-            {{ script.name }}
-          </option>
+          <option v-for="(script, index) in scripts" :key="index" :value="index">{{ script.name }}</option>
         </select>
-        <!-- <button @click="assignScript(1)" class="submit-btn">Bug Team One</button> -->
       </div>
 
-      <!-- Script selector for the second bug -->
       <div class="script-selector">
         <select v-model="selectedScriptIndex2" id="selectedScriptIndex2">
-          <option v-for="(script, index) in scripts" :key="index" :value="index">
-            {{ script.name }}
-          </option>
+          <option v-for="(script, index) in scripts" :key="index" :value="index">{{ script.name }}</option>
         </select>
-        <!-- <button @click="assignScript(2)" class="submit-btn">Bug Team Two</button> -->
       </div>
     </div>
 
-    <!-- Battle Button -->
     <button @click="startBattle">BATTLE!</button>
-
-    </div>
-
+  </div>
 </template>
+
 
 <script setup lang="ts">
 import { computed, onMounted, ref } from 'vue';
@@ -123,15 +94,15 @@ const mapCharacterToImage: { [key: string]: string | undefined } = {
   ' ': undefined,
 };
 
-const currentMapCells = computed(() => {
+  const currentMapCells = computed(() => {
   if (!gameStore.currentMap || !gameStore.currentMap.serialization) return [];
   return gameStore.currentMap.serialization.split('\n').map((row, rowIndex) =>
     row.split('').map((char, colIndex) => ({
       x: colIndex,
       y: rowIndex,
       type: char,
-      image: mapCharacterToImage[char],
-      direction: char === 'a' ? 'N' : char === 'b' ? 'E' : char === 'c' ? 'S' : 'W' // Update this logic based on your direction logic
+      image: mapCharacterToImage[char] || undefined,
+      direction: char === 'a' ? 'N' : char === 'b' ? 'E' : char === 'c' ? 'S' : 'W'
     }))
   );
 });
@@ -196,11 +167,61 @@ const startBattle = () => {
   align-items: center;
 }
 
+.game-box {
+  max-width: 80%;
+  max-height: 80%;
+}
+
 /* Game map styling */
 .game-map {
   width: 80%;
   height: 60%;
   border: 2px #003049 solid;
+  display: flex;
+  flex-direction: column;
+}
+
+.row {
+  display: flex;
+  margin: 0;
+  padding: 0;
+}
+
+.cell {
+  width: 32px; 
+  height: 32px;
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  margin: 0;
+  padding: 0;
+}
+
+.cell img {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+
+/*Bug classes for rotation*/
+.bug {
+  transition: transform 0.5s;
+}
+
+.bug.north {
+  transform: rotate(0deg);
+}
+
+.bug.east {
+  transform: rotate(90deg);
+}
+
+.bug.south {
+  transform: rotate(180deg);
+}
+
+.bug.west {
+  transform: rotate(270deg);
 }
 
 /* Button styling */
@@ -269,27 +290,6 @@ button {
 
 .time-slider::-moz-range-progress {
   background-color: rgb(212,120,44);
-}
-
-/*Bug classes for rotation*/
-.bug {
-  transition: transform 0.5s;
-}
-
-.bug.north {
-  transform: rotate(0deg);
-}
-
-.bug.east {
-  transform: rotate(90deg);
-}
-
-.bug.south {
-  transform: rotate(180deg);
-}
-
-.bug.west {
-  transform: rotate(270deg);
 }
 
 </style>
